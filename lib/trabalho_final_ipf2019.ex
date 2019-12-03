e67f260e2bcc2cd5ba549b483d33ee0d7db5224c
Jean <jeanrodrigues0908@gmail.com>
2019-12-03 20:53:59 -0300

    Processamento do campo tempo de titulacao

--- lib/trabalho_final_ipf2019.ex
+++ lib/trabalho_final_ipf2019.ex
@@ -18,20 +18,49 @@ defmodule TrabalhoFinalIpf2019 do
   end
 
   def cria_mapas_alunos(lista_alunos) do
-    [ cabecalho | alunos ] = lista_alunos
-    Enum.map(alunos, fn aluno -> Enum.zip(cabecalho, aluno) |> Map.new end )
+    [cabecalho | alunos] = lista_alunos
+
+    alunos
+    |> Enum.map(fn aluno -> Enum.zip(cabecalho, aluno) |> Map.new() end)
+  end
+
+  def filtra_alunos_desistentes(lista_alunos) do
+    lista_alunos
+    |> Enum.filter(&(&1["Situação"] == "Desistente"))
+  end
+
+  def lista_tempo_de_titulacao_em_dias(lista_mapas) do
+    lista_mapas
+    |> Enum.map(fn aluno -> processa_campo_tempo_titulacao(aluno["Tempo detitulação"]) end)
   end
 
   def processa_linha_a_linha(stream) do
     stream |> Enum.map(fn x -> String.split(x, ",") end)
   end
+
+  defp processa_campo_tempo_titulacao(tempo_titulacao) do
+    tempo_titulacao
+    |> String.replace(~r/Mes\(es\)\ e | Dia\(s\)/, "")
+    |> String.split(" ")
+    |> calcula_tempo_titulacao_dias()
+  end
+
+  defp calcula_tempo_titulacao_dias([meses | [dias | _]]) do
+    String.to_integer(meses) * 30 + String.to_integer(dias)
+  end
 end
 
 lista = TrabalhoFinalIpf2019.cria_lista_de_listas("AlunosPPGCA.csv")
 
-cabecalho = List.first(lista)
-
-um_aluno = List.last(lista)
-
-Enum.zip(cabecalho, um_aluno)
+lista
+|> TrabalhoFinalIpf2019.cria_mapas_alunos()
+|> TrabalhoFinalIpf2019.filtra_alunos_desistentes()
+|> TrabalhoFinalIpf2019.lista_tempo_de_titulacao_em_dias()
 |> IO.inspect()
+
+# cabecalho = List.first(lista)
+
+# um_aluno = List.last(lista)
+
+# Enum.zip(cabecalho, um_aluno)
+# |> IO.inspect()
