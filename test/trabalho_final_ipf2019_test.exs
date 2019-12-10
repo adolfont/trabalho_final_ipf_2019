e54ee85aa80787064a4f9c190e3d7da95dde6517
Jean <jeanrodrigues0908@gmail.com>
2019-12-10 13:45:12 -0300

    adicionado teste de media de tempo de titulacao dos alunos formados

--- test/trabalho_final_ipf2019_test.exs
+++ test/trabalho_final_ipf2019_test.exs
@@ -18,16 +18,31 @@ defmodule TrabalhoFinalIpf2019Test do
     assert codigo == "1904752"
   end
 
-  test "Calcula tempo de titulação" do
+  test "Calcula tempo de titulação formados" do
+    lista = TrabalhoFinalIpf2019.cria_lista_de_listas(@nome_arquivo)
+
+    resultado =
+      lista
+      |> TrabalhoFinalIpf2019.cria_mapas_alunos()
+      |> TrabalhoFinalIpf2019.filtra_alunos_formados()
+      |> TrabalhoFinalIpf2019.lista_tempo_de_titulacao_em_dias() 
+
+    
+    media = Enum.reduce( resultado , 0 , fn x, soma ->  x["Tempo detitulação"]  + soma end ) / length(resultado)
+    assert media == 883
+  end
+
+  test "Calcula tempo de titulação desistentes" do
     lista = TrabalhoFinalIpf2019.cria_lista_de_listas(@nome_arquivo)
 
     resultado =
       lista
       |> TrabalhoFinalIpf2019.cria_mapas_alunos()
       |> TrabalhoFinalIpf2019.filtra_alunos_desistentes()
-      |> TrabalhoFinalIpf2019.lista_tempo_de_titulacao_em_dias()
+      |> TrabalhoFinalIpf2019.lista_tempo_de_titulacao_em_dias() 
 
-    media = Enum.sum(resultado) / length(resultado)
+    
+    media = Enum.reduce( resultado , 0 , fn x, soma ->  x["Tempo detitulação"]  + soma end ) / length(resultado)
     assert media == 1710.7586206896551
   end
 end
