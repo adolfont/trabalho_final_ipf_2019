bf6aef890ffd73a399eb7e5898f8cb5063c294a5
Adolfo Neto <adolfo.usp@gmail.com>
2019-12-10 09:45:44 -0300

    testes com valores fixos adicionados

--- test/trabalho_final_ipf2019_test.exs
+++ test/trabalho_final_ipf2019_test.exs
@@ -1,4 +1,33 @@
 defmodule TrabalhoFinalIpf2019Test do
   use ExUnit.Case
   doctest TrabalhoFinalIpf2019
+
+  @nome_arquivo "AlunosPPGCA.csv"
+
+  test "Obtém último elemento" do
+    lista = TrabalhoFinalIpf2019.cria_lista_de_listas(@nome_arquivo)
+
+    cabecalho = List.first(lista)
+
+    um_aluno = List.last(lista)
+
+    ultimo = Enum.zip(cabecalho, um_aluno)
+
+    {_, codigo} = Enum.find(ultimo, fn {x, _} -> x == "Código" end)
+
+    assert codigo == "1904752"
+  end
+
+  test "Calcula tempo de titulação" do
+    lista = TrabalhoFinalIpf2019.cria_lista_de_listas(@nome_arquivo)
+
+    resultado =
+      lista
+      |> TrabalhoFinalIpf2019.cria_mapas_alunos()
+      |> TrabalhoFinalIpf2019.filtra_alunos_desistentes()
+      |> TrabalhoFinalIpf2019.lista_tempo_de_titulacao_em_dias()
+
+    media = Enum.sum(resultado) / length(resultado)
+    assert media == 1710.7586206896551
+  end
 end
