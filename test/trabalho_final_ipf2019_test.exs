2183f26068c29d210eddc0964b6331875d559545
Jean <jeanrodrigues0908@gmail.com>
2019-12-10 14:28:21 -0300

    refatoracao do teste

--- test/trabalho_final_ipf2019_test.exs
+++ test/trabalho_final_ipf2019_test.exs
@@ -25,8 +25,6 @@ defmodule TrabalhoFinalIpf2019Test do
       lista
       |> TrabalhoFinalIpf2019.cria_mapas_alunos()
       |> TrabalhoFinalIpf2019.filtra_alunos_formados()
-      |> TrabalhoFinalIpf2019.lista_tempo_de_titulacao_em_dias() 
-
     
     media = Enum.reduce( resultado , 0 , fn x, soma ->  x["Tempo detitulação"]  + soma end ) / length(resultado)
     assert media == 883
@@ -39,8 +37,6 @@ defmodule TrabalhoFinalIpf2019Test do
       lista
       |> TrabalhoFinalIpf2019.cria_mapas_alunos()
       |> TrabalhoFinalIpf2019.filtra_alunos_desistentes()
-      |> TrabalhoFinalIpf2019.lista_tempo_de_titulacao_em_dias() 
-
     
     media = Enum.reduce( resultado , 0 , fn x, soma ->  x["Tempo detitulação"]  + soma end ) / length(resultado)
     assert media == 1710.7586206896551
